2cb502473f0d9f1fddf5d070ae5bb9cfd242793a
Sujan Bhattarai <sujantimetraveller@gmail.com>
2023-09-12 08:19:48 -0700

    add a graph

---
+++ graph.py
@@ -0,0 +1,120 @@
+# coding: utf-8
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib as mp
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import seaborn as sns
+rolls = [random.randrange(1, 7) for i in range(600)]
+values, frequency = np.unique(rolls, return_counts = True)
+rolls = [random.randrange(1, 7) for i in range(600)]
+print(rolls)
+print(values)
+print(frequencies)
+print(frequency)
+#unique counts the unique values, and return_counts count occurence of each one
+values, frequency = np.unique(rolls, return_counts = True)
+title  = f"Rolling a die {len(rolls)} times"
+#unique counts the unique values, and return_counts count occurence of each one
+values, frequency = np.unique(rolls, return_counts = True)
+title  = f"Rolling a die {len(rolls)} times"
+sns.set_style("whitegrid")
+#unique counts the unique values, and return_counts count occurence of each one
+values, frequency = np.unique(rolls, return_counts = True)
+title  = f"Rolling a die {len(rolls)} times"
+sns.set_style("whitegrid")
+axes = sns.barplot(x=values, y=frequency, palette='bright')
+#unique counts the unique values, and return_counts count occurence of each one
+values, frequency = np.unique(rolls, return_counts = True)
+title  = f"Rolling a die {len(rolls)} times"
+sns.set_style("whitegrid")
+axes = sns.barplot(x=values, 
+                   y=frequency, 
+                   palette='bright')
+#unique counts the unique values, and return_counts count occurence of each one
+values, frequency = np.unique(rolls, return_counts = True)
+title  = f"Rolling a die {len(rolls)} times"
+sns.set_style("whitegrid")
+axes = sns.barplot(x=values, 
+                   y=frequency, 
+                   palette='dark')
+#unique counts the unique values, and return_counts count occurence of each one
+values, frequency = np.unique(rolls, return_counts = True)
+title  = f"Rolling a die {len(rolls)} times"
+sns.set_style("whitegrid")
+axes = sns.barplot(x=values, 
+                   y=frequency, 
+                   palette='green')
+#unique counts the unique values, and return_counts count occurence of each one
+values, frequency = np.unique(rolls, return_counts = True)
+title  = f"Rolling a die {len(rolls)} times"
+sns.set_style("whitegrid")
+axes = sns.barplot(x=values, 
+                   y=frequency, 
+                   )
+axes.set_title(title)
+#unique counts the unique values, and return_counts count occurence of each one
+values, frequency = np.unique(rolls, return_counts = True)
+title  = f"Rolling a die {len(rolls)} times"
+sns.set_style("whitegrid")
+axes = sns.barplot(x=values, 
+                   y=frequency, 
+                   )
+#unique counts the unique values, and return_counts count occurence of each one
+values, frequency = np.unique(rolls, return_counts = True)
+title  = f"Rolling a die {len(rolls)} times"
+sns.set_style("whitegrid")
+axes = sns.barplot(x=values, 
+                   y=frequency, 
+                   )
+axes.set_title(title)
+#unique counts the unique values, and return_counts count occurence of each one
+values, frequency = np.unique(rolls, return_counts = True)
+title  = f"Rolling a die {len(rolls)} times"
+sns.set_style("whitegrid")
+axes = sns.barplot(x=values, 
+                   y=frequency, 
+                   )
+axes.set_title(title)
+axes.set(xlabel ="Die value",
+         ylable = "Frequency")
+#unique counts the unique values, and return_counts count occurence of each one
+values, frequency = np.unique(rolls, return_counts = True)
+title  = f"Rolling a die {len(rolls)} times"
+sns.set_style("whitegrid")
+axes = sns.barplot(x=values, 
+                   y=frequency, 
+                   )
+axes.set_title(title)
+axes.set(xlabel ="Die value",
+         ylabel = "Frequency")
+#unique counts the unique values, and return_counts count occurence of each one
+values, frequency = np.unique(rolls, return_counts = True)
+title  = f"Rolling a die {len(rolls)} times"
+sns.set_style("whitegrid")
+axes = sns.barplot(x=values, 
+                   y=frequency, 
+                   )
+axes.set_title(title)
+axes.set(xlabel ="Die value",
+         ylabel = "Frequency")
+axes.set_ylim(top=max(frequency* 2))
+get_ipython().run_line_magic('recall', '8')
+rolls = [random.randrange(1, 7) for i in range(60000)]
+print(rolls)
+plt.cla()
+get_ipython().run_line_magic('recall', '24')
+#unique counts the unique values, and return_counts count occurence of each one
+values, frequency = np.unique(rolls, return_counts = True)
+title  = f"Rolling a die {len(rolls)} times"
+sns.set_style("whitegrid")
+axes = sns.barplot(x=values, 
+                   y=frequency, 
+                   )
+axes.set_title(title)
+axes.set(xlabel ="Die value",
+         ylabel = "Frequency")
+axes.set_ylim(top=max(frequency* 2))
